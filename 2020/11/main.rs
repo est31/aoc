57a40fd70dfe499fe9b0161942840e8d7efa8f6f
est31 <MTest31@outlook.com>
2022-10-24 15:59:51 +0200

    Day 2020/11 part 2

--- 2020/11/main.rs
+++ 2020/11/main.rs
@@ -5,7 +5,8 @@ mod test;
 
 fn main() {
 	let fields = parse(INPUT);
-	println!("Occupied seats after fixpoint reached: {}", step_until_no_change(fields));
+	println!("Occupied seats after fixpoint reached: {}", step_until_no_change(&fields, step));
+	println!("Modified rules: {}", step_until_no_change(&fields, step_p2));
 }
 
 #[derive(Copy, Clone, PartialEq, Eq)]
@@ -59,7 +60,7 @@ fn fields_to_string(fields :&[Vec<Field>]) -> String {
 
 fn count_occupied_adjacent(fields :&[Vec<Field>], i :usize, j :usize) -> usize {
 	fn range(v :usize, limit :usize) -> Vec<usize> {
-		let mut res = Vec::with_capacity(2);
+		let mut res = Vec::with_capacity(3);
 		if v > 0 {
 			res.push(v - 1);
 		}
@@ -85,31 +86,37 @@ fn count_occupied_adjacent(fields :&[Vec<Field>], i :usize, j :usize) -> usize {
 	sum
 }
 
-fn step(fields :&[Vec<Field>]) -> (bool, Vec<Vec<Field>>) {
+fn map_fields(fields :&[Vec<Field>], f :impl Fn(usize, usize, Field) -> (bool, Field)) -> (bool, Vec<Vec<Field>>) {
 	let mut change = false;
 	let res = fields.iter()
 		.enumerate()
 		.map(|(i, l)| {
 			l.iter()
 				.enumerate()
-				.map(|(j, f)| {
-					let occupied_adj = count_occupied_adjacent(fields, i, j);
-					match (f, occupied_adj) {
-						(Field::EmptySeat, 0) => {
-							change = true;
-							Field::FullSeat
-						},
-						(Field::FullSeat, 4..) => {
-							change = true;
-							Field::EmptySeat
-						},
-						_ => *f,
-					}
+				.map(|(j, field)| {
+					let (ch, field) = f(i, j, *field);
+					change |= ch;
+					field
 				})
 				.collect::<Vec<_>>()
 		})
 		.collect::<Vec<_>>();
 	(change, res)
+}
+
+fn step(fields :&[Vec<Field>]) -> (bool, Vec<Vec<Field>>) {
+	map_fields(fields, |i, j, field| {
+		let occupied_adj = count_occupied_adjacent(fields, i, j);
+		match (field, occupied_adj) {
+			(Field::EmptySeat, 0) => {
+				(true, Field::FullSeat)
+			},
+			(Field::FullSeat, 4..) => {
+				(true, Field::EmptySeat)
+			},
+			_ => (false, field),
+		}
+	})
 }
 
 fn count_occupied(fields :&[Vec<Field>]) -> usize {
@@ -122,10 +129,62 @@ fn count_occupied(fields :&[Vec<Field>]) -> usize {
 		.sum()
 }
 
-fn step_until_no_change(fields :Vec<Vec<Field>>) -> usize {
-	let mut fields = fields;
+
+fn count_occupied_dirs(fields :&[Vec<Field>], i :usize, j :usize) -> usize {
+	let dirs = [
+		(-1, -1), (0, -1), (1, -1),
+		(-1, 0), (1, 0),
+		(-1, 1), (0, 1), (1, 1),
+	];
+	let mut sum = 0;
+	for dir in dirs {
+		let mut i = i as isize;
+		let mut j = j as isize;
+
+		loop {
+			i += dir.0;
+			j += dir.1;
+
+			if i < 0 || j < 0 {
+				break;
+			}
+			if i >= fields.len() as isize || j >= fields[0].len() as isize {
+				break;
+			}
+			let f = fields[i as usize][j as usize];
+			match f {
+				Field::FullSeat => {
+					sum += 1;
+					break;
+				},
+				Field::EmptySeat => break,
+				Field::Floor => (),
+			}
+		}
+	}
+	sum
+}
+
+fn step_p2(fields :&[Vec<Field>]) -> (bool, Vec<Vec<Field>>) {
+	map_fields(fields, |i, j, field| {
+		let occupied_adj = count_occupied_dirs(fields, i, j);
+		match (field, occupied_adj) {
+			(Field::EmptySeat, 0) => {
+				(true, Field::FullSeat)
+			},
+			(Field::FullSeat, 5..) => {
+				(true, Field::EmptySeat)
+			},
+			_ => (false, field),
+		}
+	})
+}
+
+
+fn step_until_no_change(fields :&[Vec<Field>], step_fn :fn(&[Vec<Field>]) -> (bool, Vec<Vec<Field>>)) -> usize {
+	let mut fields = fields.to_vec();
 	loop {
-		let (changes, new_fields) = step(&fields);
+		let (changes, new_fields) = step_fn(&fields);
 		if !changes {
 			break;
 		}
--- 2020/11/test.rs
+++ 2020/11/test.rs
@@ -107,5 +107,101 @@ fn test_1() {
 	assert_eq!(EXAMPLE_INPUT_5, fields_to_string(&fields));
 	assert_eq!(false, changes);
 
-	assert_eq!(37, step_until_no_change(start_fields));
+	assert_eq!(37, step_until_no_change(&start_fields, step));
+}
+
+const EXAMPLE_INPUT_2_2 :&str = "\
+#.LL.LL.L#
+#LLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLL#
+#.LLLLLL.L
+#.LLLLL.L#
+";
+
+const EXAMPLE_INPUT_2_3 :&str = "\
+#.L#.##.L#
+#L#####.LL
+L.#.#..#..
+##L#.##.##
+#.##.#L.##
+#.#####.#L
+..#.#.....
+LLL####LL#
+#.L#####.L
+#.L####.L#
+";
+
+const EXAMPLE_INPUT_2_4 :&str = "\
+#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##LL.LL.L#
+L.LL.LL.L#
+#.LLLLL.LL
+..L.L.....
+LLLLLLLLL#
+#.LLLLL#.L
+#.L#LL#.L#
+";
+
+const EXAMPLE_INPUT_2_5 :&str = "\
+#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##L#.#L.L#
+L.L#.#L.L#
+#.L####.LL
+..#.#.....
+LLL###LLL#
+#.LLLLL#.L
+#.L#LL#.L#
+";
+
+const EXAMPLE_INPUT_2_6 :&str = "\
+#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##L#.#L.L#
+L.L#.LL.L#
+#.LLLL#.LL
+..#.L.....
+LLL###LLL#
+#.LLLLL#.L
+#.L#LL#.L#
+";
+
+#[test]
+fn test_2() {
+	let start_fields = parse(EXAMPLE_INPUT);
+	let s = fields_to_string(&start_fields);
+	assert_eq!(EXAMPLE_INPUT, s);
+
+	let fields = step_p2(&start_fields).1;
+	assert_eq!(EXAMPLE_INPUT_1, fields_to_string(&fields));
+
+	let fields = step_p2(&fields).1;
+	assert_eq!(EXAMPLE_INPUT_2_2, fields_to_string(&fields));
+
+	let fields = step_p2(&fields).1;
+	assert_eq!(EXAMPLE_INPUT_2_3, fields_to_string(&fields));
+
+	let fields = step_p2(&fields).1;
+	assert_eq!(EXAMPLE_INPUT_2_4, fields_to_string(&fields));
+
+	let fields = step_p2(&fields).1;
+	assert_eq!(EXAMPLE_INPUT_2_5, fields_to_string(&fields));
+
+	let fields = step_p2(&fields).1;
+	assert_eq!(EXAMPLE_INPUT_2_6, fields_to_string(&fields));
+
+	let (changes, fields) = step_p2(&fields);
+	assert_eq!(EXAMPLE_INPUT_2_6, fields_to_string(&fields));
+	assert_eq!(false, changes);
+
+	assert_eq!(26, step_until_no_change(&start_fields, step_p2));
 }
