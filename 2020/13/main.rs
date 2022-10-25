ea4b1b87e021dbb258f331a15bff7ed7b208b923
est31 <MTest31@outlook.com>
2022-10-25 03:19:17 +0200

    Day 2020/13 part 2

--- 2020/13/main.rs
+++ 2020/13/main.rs
@@ -9,6 +9,8 @@ fn main() {
 	let (ts, vals) = parse(INPUT);
 	let (id, wt) = min_bus_time(ts, &vals);
 	println!("Product: {}", id * wt);
+	let c = min_bus_cascade(&vals);
+	println!("First cascade time: {}", c);
 }
 
 fn parse(input :&str) -> (u32, Vec<u32>) {
@@ -17,7 +19,6 @@ fn parse(input :&str) -> (u32, Vec<u32>) {
 	let l1 = lines.next().unwrap();
 	let v = u32::from_str(l0).unwrap();
 	let w = l1.split(',')
-		.filter(|b| *b != "x")
 		.map(|b| match b {
 			"x" => 0,
 			_ => u32::from_str(b).unwrap()
@@ -44,4 +45,37 @@ fn min_bus_time(ts :u32, vals :&[u32]) -> (u32, u32) {
 		})
 		.unwrap();
 	(v, wt)
+}
+
+fn min_bus_cascade(vals :&[u32]) -> u64 {
+	let mut vals_dist = Vec::new();
+	let mut dist = 0;
+	for val in vals {
+		if *val > 0 {
+			let val = u64::from(*val);
+			let offs = if dist % val == 0 {
+				0
+			} else {
+				val.checked_sub(dist % val).unwrap()
+			};
+			vals_dist.push((val, offs));
+		}
+		dist += 1;
+	}
+	let mut mul = vals_dist[0].0;
+	let mut offs = 0;
+	for &(val, dist) in &vals_dist[1..] {
+		//println!("val={val}, mul={mul}, offs={offs}");
+		for m in 0.. {
+			let v = (m * mul) + offs;
+			if v % val == dist {
+				//println!("    -> m={m}, v={v}, dist={dist}");
+				offs = v;
+				mul *= val;
+				break;
+			}
+		}
+	}
+	//println!("-> {offs}");
+	offs
 }
--- 2020/13/test.rs
+++ 2020/13/test.rs
@@ -9,8 +9,24 @@ const EXAMPLE_INPUT :&str = "\
 fn test_1() {
 	let p = parse(EXAMPLE_INPUT);
 	assert_eq!(939, p.0);
-	assert_eq!(&[7, 13, 0, 0, 59, 31, 19], p.1.as_slice());
+	assert_eq!(&[7, 13, 0, 0, 59, 0, 31, 19], p.1.as_slice());
 	let (id, wt) = min_bus_time(p.0, &p.1);
 	assert_eq!(59, id);
 	assert_eq!(5, wt);
+}
+
+#[test]
+fn test_2() {
+	let p = parse(EXAMPLE_INPUT);
+	assert_eq!(1068781, min_bus_cascade(&p.1));
+	let p = parse("0\n17,x,13,19");
+	assert_eq!(3417, min_bus_cascade(&p.1));
+	let p = parse("0\n67,7,59,61");
+	assert_eq!(754018, min_bus_cascade(&p.1));
+	let p = parse("0\n67,x,7,59,61");
+	assert_eq!(779210, min_bus_cascade(&p.1));
+	let p = parse("0\n67,7,x,59,61");
+	assert_eq!(1261476, min_bus_cascade(&p.1));
+	let p = parse("0\n1789,37,47,1889");
+	assert_eq!(1202161486, min_bus_cascade(&p.1));
 }
