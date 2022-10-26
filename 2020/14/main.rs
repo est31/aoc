dd7343375e4bdbbbe855f109eacc8643ca1f860d
est31 <MTest31@outlook.com>
2022-10-26 14:49:26 +0200

    Day 2020/14 part 2

--- 2020/14/main.rs
+++ 2020/14/main.rs
@@ -10,11 +10,13 @@ fn main() {
 	let cmds = parse(INPUT);
 	let sum = apply_cmds(&cmds);
 	println!("Sum: {sum}");
+	let sum = apply_cmds_v2_slow(&cmds);
+	println!("Sum v2: {sum}");
 }
 
 #[derive(Copy, Clone, Debug)]
 enum Cmd {
-	Mask(u64, u64),
+	Mask(u64, u64, u64),
 	Mem(u64, u64),
 }
 
@@ -28,15 +30,16 @@ fn parse(input :&str) -> Vec<Cmd> {
 			if first == "mask" {
 				let mut ones = 0;
 				let mut zeroes = 0;
+				let mut exes = 0;
 				for (i, ch) in second.chars().enumerate() {
 					match ch {
 						'0' => zeroes |= 1 << (35 - i),
 						'1' => ones |= 1 << (35 - i),
-						'X' => (),
+						'X' => exes |= 1 << (35 - i),
 						_ => panic!("Unexpected char '{ch}'!"),
 					}
 				}
-				Cmd::Mask(ones, zeroes)
+				Cmd::Mask(ones, zeroes, exes)
 			} else {
 				let addr_str = first.split(['[', ']'])
 					.nth(1)
@@ -62,10 +65,54 @@ fn apply_cmds(cmds :&[Cmd]) -> u64 {
 				//println!("  {addr} <- {c} {c:b} (originally {content})");
 				memory.insert(addr, c);
 			},
-			Cmd::Mask(ones, zeroes) => {
+			Cmd::Mask(ones, zeroes, _exes) => {
 				//println!("  {ones:b} {zeroes:b}");
 				mask_ones = *ones;
 				mask_zeroes = *zeroes;
+			},
+		}
+	}
+	memory.iter()
+		.map(|(_addr, content)| content)
+		.sum()
+}
+
+fn for_exes(addr :u64, exes :u64, offs :usize, f :&mut impl FnMut(u64)) {
+	if offs >= 36 {
+		f(addr);
+		return;
+	}
+	let mut offs = offs;
+	while (exes >> offs) & 1 == 0 {
+		offs += 1;
+		if offs > 36 {
+			f(addr);
+			return;
+		}
+	}
+	for_exes(addr | (1 << offs), exes, offs + 1, f);
+	for_exes(addr & !(1 << offs), exes, offs + 1, f);
+
+}
+
+fn apply_cmds_v2_slow(cmds :&[Cmd]) -> u64 {
+	let mut mask_ones = 0;
+	let mut mask_exes = 0;
+	let mut memory = HashMap::new();
+	for cmd in cmds {
+		//println!("{cmd:?}");
+		match cmd {
+			Cmd::Mem(addr, content) => {
+				let addr = addr | mask_ones;
+				//println!("  {addr} <- {c} {c:b} (originally {content})");
+				for_exes(addr, mask_exes, 0, &mut |addr| {
+					memory.insert(addr, *content);
+				});
+			},
+			Cmd::Mask(ones, _zeroes, exes) => {
+				//println!("  {ones:b} {_zeroes:b} {exes:b}");
+				mask_ones = *ones;
+				mask_exes = *exes;
 			},
 		}
 	}
--- 2020/14/test.rs
+++ 2020/14/test.rs
@@ -1,15 +1,29 @@
 use super::*;
 
-const EXAMPLE_INPUT :&str = "\
+const EXAMPLE_INPUT_1 :&str = "\
 mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
 mem[8] = 11
 mem[7] = 101
 mem[8] = 0
 ";
 
+const EXAMPLE_INPUT_2 :&str = "\
+mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1
+";
+
 #[test]
 fn test_1() {
-	let cmds = parse(EXAMPLE_INPUT);
+	let cmds = parse(EXAMPLE_INPUT_1);
 	let sum = apply_cmds(&cmds);
 	assert_eq!(sum, 165);
+}
+
+#[test]
+fn test_2() {
+	let cmds = parse(EXAMPLE_INPUT_2);
+	let sum = apply_cmds_v2_slow(&cmds);
+	assert_eq!(sum, 208);
 }
