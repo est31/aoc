5e34f6b45abe18db54827b8538fabbfb4cce2575
est31 <MTest31@outlook.com>
2022-10-26 21:26:45 +0200

    Day 2020/15 part 1

---
+++ 2020/15/main.rs
@@ -0,0 +1,46 @@
+use std::collections::HashMap;
+use std::str::FromStr;
+
+const INPUT :&str = include_str!("input");
+
+#[cfg(test)]
+mod test;
+
+fn main() {
+	let nums = parse(INPUT);
+	let num = play_game(&nums);
+	println!("Num: {num}");
+}
+
+fn parse(input :&str) -> Vec<u32> {
+	input.trim()
+		.split(',')
+		.map(|n| u32::from_str(n).unwrap())
+		.collect::<Vec<_>>()
+}
+
+fn play_game(nums :&[u32]) -> u32 {
+	play_game_n(nums, 2020)
+}
+
+fn play_game_n(nums :&[u32], limit :usize) -> u32 {
+	let mut times = HashMap::new();
+	let mut last_cont = None;
+	let mut last = 0;
+	for (i, n) in nums.iter().enumerate() {
+		last_cont = times.insert(*n, i);
+		last = *n;
+	}
+	for i in nums.len()..limit {
+		let cur = if let Some(lt) = last_cont {
+			(i - lt - 1) as u32
+		} else {
+			0
+		};
+		last_cont = times.insert(cur, i);
+		//println!("{cur}");
+		last = cur;
+	}
+	//println!("");
+	last
+}
---
+++ 2020/15/test.rs
@@ -0,0 +1,24 @@
+use super::*;
+
+#[test]
+fn test_1() {
+	let nums = parse("0,3,6");
+	assert_eq!(play_game_n(&nums, 4), 0);
+	assert_eq!(play_game_n(&nums, 5), 3);
+	assert_eq!(play_game_n(&nums, 6), 3);
+	assert_eq!(play_game_n(&nums, 7), 1);
+	assert_eq!(play_game_n(&nums, 8), 0);
+	assert_eq!(play_game_n(&nums, 9), 4);
+	assert_eq!(play_game_n(&nums, 10), 0);
+
+	assert_eq!(play_game(&parse("0,3,6")), 436);
+}
+#[test]
+fn test_2() {
+	assert_eq!(play_game(&parse("1,3,2")), 1);
+	assert_eq!(play_game(&parse("2,1,3")), 10);
+	assert_eq!(play_game(&parse("1,2,3")), 27);
+	assert_eq!(play_game(&parse("2,3,1")), 78);
+	assert_eq!(play_game(&parse("3,2,1")), 438);
+	assert_eq!(play_game(&parse("3,1,2")), 1836);
+}
