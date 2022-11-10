9e5d8906f872f83da70f4f99eaac704d95148462
est31 <MTest31@outlook.com>
2022-11-10 03:59:53 +0100

    Day 2020/22 part 2

--- 2020/22/main.rs
+++ 2020/22/main.rs
@@ -1,3 +1,4 @@
+use std::collections::{HashSet, HashMap};
 use std::str::FromStr;
 
 const INPUT :&str = include_str!("input");
@@ -9,6 +10,8 @@ fn main() {
 	let cards = parse(INPUT);
 	let score = winner_score(&cards.0, &cards.1);
 	println!("Winner score: {score}");
+	let score_rec = winner_score_recursive(&cards.0, &cards.1);
+	println!("Winner score recursive: {score_rec}");
 }
 
 fn parse(input :&str) -> (Vec<u16>, Vec<u16>) {
@@ -62,4 +65,53 @@ fn winner_score(p1 :&[u16], p2 :&[u16]) -> u16 {
 		p1
 	};
 	score(&winner)
+}
+
+fn winner_recursive(results :&mut HashMap<(Vec<u16>, Vec<u16>), (bool, u16)>, p1 :&[u16], p2 :&[u16]) -> (bool, u16) {
+	let p_orig = (p1.to_vec(), p2.to_vec());
+	if let Some(res) = results.get(&p_orig) {
+		return *res;
+	}
+
+	let mut p1 = p1.to_vec();
+	let mut p2 = p2.to_vec();
+	let mut encountered = HashSet::new();
+	while p1.len() != 0 && p2.len() != 0 {
+		if encountered.contains(&(p1.clone(), p2.clone())) {
+			let res = (true, score(&p1));
+			results.insert(p_orig, res);
+			return res;
+		} else {
+			encountered.insert((p1.clone(), p2.clone()));
+		}
+		let p1_card = p1.remove(0);
+		let p2_card = p2.remove(0);
+		let p1_won = if p1_card as usize <= p1.len() && p2_card as usize <= p2.len() {
+			let p1 = &p1[..p1_card as usize];
+			let p2 = &p2[..p2_card as usize];
+			winner_recursive(results, p1, p2).0
+		} else {
+			p1_card > p2_card
+		};
+		let (winner, cards) = if p1_won {
+			(&mut p1, (p1_card, p2_card))
+		} else {
+			(&mut p2, (p2_card, p1_card))
+		};
+		winner.push(cards.0);
+		winner.push(cards.1);
+	}
+	let (wb, winner) = if p1.len() == 0 {
+		(false, p2)
+	} else {
+		(true, p1)
+	};
+	let res = (wb, score(&winner));
+	results.insert(p_orig, res);
+	res
+}
+
+fn winner_score_recursive(p1 :&[u16], p2 :&[u16]) -> u16 {
+	let mut results = HashMap::new();
+	winner_recursive(&mut results, p1, p2).1
 }
--- 2020/22/test.rs
+++ 2020/22/test.rs
@@ -25,4 +25,28 @@ fn test_1() {
 #[test]
 fn test_score() {
 	assert_eq!(score(&[3, 2, 10, 6, 8, 5, 9, 4, 7, 1]), 306);
+	assert_eq!(score(&[7, 5, 6, 2, 4, 1, 10, 8, 9, 3]), 291);
+}
+
+#[test]
+fn test_2() {
+	let (p1, p2) = parse(EXAMPLE_INPUT_1);
+	assert_eq!(winner_score_recursive(&p1, &p2), 291);
+}
+
+const EXAMPLE_INPUT_2 :&str = "\
+Player 1:
+43
+19
+
+Player 2:
+2
+29
+14
+";
+
+#[test]
+fn test_termination() {
+	let (p1, p2) = parse(EXAMPLE_INPUT_2);
+	winner_score_recursive(&p1, &p2);
 }
