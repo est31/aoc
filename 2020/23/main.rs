cefe17d931aeeb76d6ade693d93a4ed261f6f9d5
est31 <MTest31@outlook.com>
2022-11-23 22:01:57 +0100

    Slow and naive implementation of part 2
    I think they *really* want us to use linked lists...

--- 2020/23/main.rs
+++ 2020/23/main.rs
@@ -19,14 +19,23 @@ fn parse(input :&str) -> Vec<u32> {
 struct Cups {
 	current :usize,
 	cups :Vec<u32>,
+	biggest_cup_num :u32,
 }
 
 impl Cups {
 	fn new(cups :Vec<u32>) -> Self {
+		let biggest_cup_num = *cups.iter().max().unwrap();
 		Self {
 			current : 0,
 			cups,
+			biggest_cup_num,
 		}
+	}
+	fn add_until_million(&mut self) {
+		for c in self.cups.len()..1_000_000 {
+			self.cups.push(c as u32);
+		}
+		self.biggest_cup_num = 1_000_000 - 1;
 	}
 	fn do_move(&mut self) {
 		let len = self.cups.len();
@@ -44,7 +53,7 @@ impl Cups {
 		let mut dest_idx = None;
 		while dest_idx.is_none() {
 			if tgt == 0 {
-				tgt = *self.cups.iter().max().unwrap();
+				tgt = self.biggest_cup_num;
 			}
 			let found = self.cups.iter().enumerate().find(|(_i, c)| c == &&tgt);
 			if let Some((tgt_idx, _tgt)) = found {
@@ -92,6 +101,18 @@ impl Cups {
 		}
 		res
 	}
+	fn get_labels_product_right_of_one(&self) -> u64 {
+		let idx_of_one = self.cups.iter()
+			.enumerate()
+			.find(|(_, num)| **num == 1)
+			.unwrap()
+			.0;
+		let next = (idx_of_one + 1) % self.cups.len();
+		let next2 = (idx_of_one + 2) % self.cups.len();
+		let next = self.cups[next] as u64;
+		let next2 = self.cups[next2] as u64;
+		next * next2
+	}
 }
 
 fn get_labels_after_n(nums :&[u32], n: u16) -> String {
@@ -104,4 +125,16 @@ fn get_labels_after_n(nums :&[u32], n: u16) -> String {
 
 fn get_labels_after_100(nums :&[u32]) -> String {
 	get_labels_after_n(nums, 100)
+}
+
+fn get_labels_after_ten_million(nums :&[u32]) -> u64 {
+	let mut cups = Cups::new(nums.to_vec());
+	cups.add_until_million();
+	for _ctr in 0..10_000_000u64 {
+		if _ctr % 10_000 == 0 {
+			println!("{_ctr} => {:.2}", (_ctr as f64 / 10_000_000.0));
+		}
+		cups.do_move();
+	}
+	cups.get_labels_product_right_of_one()
 }
--- 2020/23/test.rs
+++ 2020/23/test.rs
@@ -8,7 +8,18 @@ fn test_1() {
 }
 
 #[test]
+fn test_2() {
+	let nums = parse("389125467");
+	assert_eq!(get_labels_after_ten_million(&nums), 149245887792);
+}
+
+
+#[test]
 fn test_get_labels() {
-	let cups = Cups { current: 0, cups: vec![5, 8, 3, 7, 4, 1, 9, 2, 6], };
+	let cups = Cups {
+		cups: vec![5, 8, 3, 7, 4, 1, 9, 2, 6],
+		biggest_cup_num: 9,
+		current: 0,
+	};
 	assert_eq!("92658374", cups.get_labels());
 }
