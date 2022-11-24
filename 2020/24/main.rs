90c011bac743eb4a71af757aa2127908059fcd78
est31 <MTest31@outlook.com>
2022-11-24 19:39:47 +0100

    Day 2020/24 part 2

--- 2020/24/main.rs
+++ 2020/24/main.rs
@@ -7,8 +7,10 @@ mod test;
 
 fn main() {
 	let cmds = parse(INPUT);
-	let flipped_count = do_flips(&cmds);
-	println!("Flipped count: {flipped_count}");
+	let flipped = do_flips(&cmds);
+	println!("Flipped count: {}", flipped.len());
+	let flipped_after = n_days(100, flipped);
+	println!("Flipped after 100 days: {}", flipped_after.len());
 }
 
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
@@ -89,7 +91,7 @@ fn coords_for_cmds(line :&[Dir]) -> (i16, i16) {
 	(x, y)
 }
 
-fn do_flips(cmds :&[Vec<Dir>]) -> u64 {
+fn do_flips(cmds :&[Vec<Dir>]) -> HashSet<(i16, i16)> {
 	let mut flipped = HashSet::new();
 	for line in cmds {
 		let coords = coords_for_cmds(line);
@@ -99,5 +101,53 @@ fn do_flips(cmds :&[Vec<Dir>]) -> u64 {
 			flipped.insert(coords);
 		}
 	}
-	flipped.len() as u64
+	flipped
+}
+
+fn neighs_contained(x :i16, y :i16, hs :&HashSet<(i16, i16)>) -> u8 {
+	let mut ret = 0;
+	// NorthEast
+	ret += hs.contains(&(x - 1, y + 1)) as u8;
+	// NorthWest
+	ret += hs.contains(&(x, y + 1)) as u8;
+	// East
+	ret += hs.contains(&(x - 1, y)) as u8;
+	// West
+	ret += hs.contains(&(x + 1, y)) as u8;
+	// SouthEast
+	ret += hs.contains(&(x, y - 1)) as u8;
+	// SouthWest
+	ret += hs.contains(&(x + 1, y - 1)) as u8;
+	ret
+}
+
+fn day(old :HashSet<(i16, i16)>) -> HashSet<(i16, i16)> {
+	let x_min = old.iter().map(|(x, _y)| x).min().unwrap() - 1;
+	let x_max = old.iter().map(|(x, _y)| x).max().unwrap() + 1;
+	let y_min = old.iter().map(|(_x, y)| y).min().unwrap() - 1;
+	let y_max = old.iter().map(|(_x, y)| y).max().unwrap() + 1;
+	let mut new = HashSet::new();
+	for x in x_min..=x_max {
+		for y in y_min..=y_max {
+			let cont = neighs_contained(x, y, &old);
+			let was_black = old.contains(&(x, y));
+			if was_black && (cont == 0 || cont > 2) {
+				// Flip to white
+				continue;
+			}
+			if was_black || cont == 2 {
+				// Tile will be black
+				new.insert((x, y));
+			}
+		}
+	}
+	new
+}
+
+fn n_days(n :usize, old :HashSet<(i16, i16)>) -> HashSet<(i16, i16)> {
+	let mut hs = old;
+	for _ in 0..n {
+		hs = day(hs);
+	}
+	hs
 }
--- 2020/24/test.rs
+++ 2020/24/test.rs
@@ -34,6 +34,32 @@ wseweeenwnesenwwwswnew
 #[test]
 fn test_1() {
 	let cmds = parse(EXAMPLE_INPUT_1);
-	assert_eq!(do_flips(&cmds), 10);
+	assert_eq!(do_flips(&cmds).len(), 10);
 }
 
+#[test]
+fn test_2() {
+	let cmds = parse(EXAMPLE_INPUT_1);
+	let flipped = do_flips(&cmds);
+	assert_eq!(flipped.len(), 10);
+	assert_eq!(n_days(1, flipped.clone()).len(), 15);
+	assert_eq!(n_days(2, flipped.clone()).len(), 12);
+	assert_eq!(n_days(3, flipped.clone()).len(), 25);
+	assert_eq!(n_days(4, flipped.clone()).len(), 14);
+	assert_eq!(n_days(5, flipped.clone()).len(), 23);
+	assert_eq!(n_days(6, flipped.clone()).len(), 28);
+	assert_eq!(n_days(7, flipped.clone()).len(), 41);
+	assert_eq!(n_days(8, flipped.clone()).len(), 37);
+	assert_eq!(n_days(9, flipped.clone()).len(), 49);
+	assert_eq!(n_days(10, flipped.clone()).len(), 37);
+	// ...
+	assert_eq!(n_days(20, flipped.clone()).len(), 132);
+	assert_eq!(n_days(30, flipped.clone()).len(), 259);
+	assert_eq!(n_days(40, flipped.clone()).len(), 406);
+	assert_eq!(n_days(50, flipped.clone()).len(), 566);
+	assert_eq!(n_days(60, flipped.clone()).len(), 788);
+	assert_eq!(n_days(70, flipped.clone()).len(), 1106);
+	assert_eq!(n_days(80, flipped.clone()).len(), 1373);
+	assert_eq!(n_days(90, flipped.clone()).len(), 1844);
+	assert_eq!(n_days(100, flipped.clone()).len(), 2208);
+}
