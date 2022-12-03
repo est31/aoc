578fc0e33b61353485efc2c2b1bbaba0c1fa1c7d
est31 <MTest31@outlook.com>
2022-12-03 21:41:40 +0100

    Day 2022/03 part 1

---
+++ 2022/03/main.rs
@@ -0,0 +1,50 @@
+use std::collections::HashSet;
+
+const INPUT :&str = include_str!("input");
+
+#[cfg(test)]
+mod test;
+
+fn main() {
+	let lines = parse(INPUT);
+	let prios = priorities_of_dupes(&lines);
+	println!("priorities: {prios}");
+}
+
+fn parse(input :&str) -> Vec<(&str, &str)> {
+	input.lines()
+		.map(|l| {
+			let len = l.len();
+			assert_eq!(len % 2, 0, "len {len} is not even");
+			let hlen = len / 2;
+			let comp_1 = &l[0..hlen];
+			let comp_2 = &l[hlen..];
+			(comp_1, comp_2)
+		})
+		.collect::<Vec<_>>()
+}
+
+fn priority(ch :char) -> u16 {
+	let p = match ch {
+		'a'..='z' => ch as u8 - b'a' + 1,
+		'A'..='Z' => ch as u8 - b'A' + 27,
+		_ => panic!("invalid char '{ch}'"),
+	};
+	p.into()
+}
+
+fn priorities_of_dupes(lines :&[(&str, &str)]) -> u16 {
+	lines.iter()
+		.map(|(c1, c2)| {
+			let hs :HashSet<char> = c1.chars().collect();
+			let dupes = c2.chars()
+				.filter(|ch| hs.contains(ch))
+				.map(|ch| priority(ch))
+				.collect::<Vec<_>>();
+			if dupes.iter().any(|d| *d != dupes[0]) {
+				panic!("Multiple dupes found in '{c1}' '{c2}'");
+			}
+			dupes[0]
+		})
+		.sum()
+}
---
+++ 2022/03/test.rs
@@ -0,0 +1,28 @@
+use super::*;
+
+const EXAMPLE_INPUT :&str = "\
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+";
+
+#[test]
+fn test_1() {
+	let lines = parse(EXAMPLE_INPUT);
+	assert_eq!(lines[0].0, "vJrwpWtwJgWr");
+	assert_eq!(lines[0].1, "hcsFMMfFFhFp");
+	assert_eq!(lines[1].0, "jqHRNqRjqzjGDLGL");
+	assert_eq!(lines[1].1, "rsFMfFZSrLrFZsSL");
+	assert_eq!(lines[2].0, "PmmdzqPrV");
+	assert_eq!(lines[2].1, "vPwwTWBwg");
+
+	assert_eq!(priorities_of_dupes(&lines[..1]), 16);
+	assert_eq!(priorities_of_dupes(&lines[..2]), 16 + 38);
+	assert_eq!(priorities_of_dupes(&lines[..3]), 16 + 38 + 42);
+
+	let prios = priorities_of_dupes(&lines);
+	assert_eq!(prios, 157);
+}
