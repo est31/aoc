97b3b7466b7624fc6895ccfa4d3d1087d6de2972
est31 <MTest31@outlook.com>
2022-12-09 07:18:00 +0100

    Day 2022/09 part 2 but it crashes on the input
    The tests work but the given input gives a panic
    for invalid x_diff/y_diff.

--- 2022/09/main.rs
+++ 2022/09/main.rs
@@ -8,8 +8,10 @@ mod test;
 
 fn main() {
 	let grid = parse(INPUT);
-	let ov = visited_positions(&grid);
-	println!("visited positions: {}", ov);
+	let ov = visited_positions(&grid, 1);
+	println!("visited positions len 2: {}", ov);
+	let ov = visited_positions(&grid, 9);
+	println!("visited positions len 10: {}", ov);
 }
 
 fn parse(input :&str) -> Vec<(char, u32)> {
@@ -29,13 +31,42 @@ fn parse(input :&str) -> Vec<(char, u32)> {
 		.collect::<Vec<_>>()
 }
 
-fn visited_positions(cmds :&[(char, u32)]) -> usize {
+fn modify_for_head(hx :i32, hy :i32, tx :&mut i32, ty :&mut i32) -> bool {
+	let x_diff = (hx - *tx).abs();
+	let y_diff = (hy - *ty).abs();
+	if x_diff.max(y_diff) > 1 {
+		match (x_diff, y_diff) {
+			(2, 1) => {
+				// Move diagonally
+				*tx += (hx - *tx).signum();
+				*ty = hy;
+			},
+			(1, 2) => {
+				// Move diagonally
+				*tx = hx;
+				*ty += (hy - *ty).signum();
+			},
+			(2, 2) => {
+				// Move diagonally
+				*tx = (hx - *tx).signum();
+				*ty += (hy - *ty).signum();
+			},
+			(2, 0) => *tx += (hx - *tx).signum(),
+			(0, 2) => *ty += (hy - *ty).signum(),
+			_ => panic!("invalid x_diff={x_diff}, y_diff={y_diff}"),
+		}
+		true
+	} else {
+		false
+	}
+}
+
+fn visited_positions(cmds :&[(char, u32)], tail_len :usize) -> usize {
 	let mut visited = HashSet::new();
 	let mut hx = 0i32;
 	let mut hy = 0i32;
-	let mut tx = 0i32;
-	let mut ty = 0i32;
-	visited.insert((tx, ty));
+	let mut tp = vec![(0i32, 0i32); tail_len];
+	visited.insert(*tp.last().unwrap());
 	for (dir, num) in cmds.iter() {
 		let (dx, dy) = match dir {
 			'R' => (0, 1),
@@ -47,25 +78,17 @@ fn visited_positions(cmds :&[(char, u32)]) -> usize {
 		for _ in 0..*num {
 			hx += dx;
 			hy += dy;
-			let x_diff = (hx - tx).abs();
-			let y_diff = (hy - ty).abs();
-			if x_diff.max(y_diff) > 1 {
-				match (x_diff, y_diff) {
-					(2, 1) => {
-						// Move diagonally
-						tx += (hx - tx).signum();
-						ty = hy;
-					},
-					(1, 2) => {
-						// Move diagonally
-						tx = hx;
-						ty += (hy - ty).signum();
-					},
-					(2, 0) => tx += dx,
-					(0, 2) => ty += dy,
-					_ => panic!("invalid x_diff={x_diff}, y_diff={y_diff}"),
+			let mut rhx = hx;
+			let mut rhy = hy;
+			let tp_len = tp.len();
+			for (i, (rtx, rty)) in tp.iter_mut().enumerate() {
+				if modify_for_head(rhx, rhy, rtx, rty) {
+					if i == tp_len - 1 {
+						visited.insert((*rtx, *rty));
+					}
 				}
-				visited.insert((tx, ty));
+				rhx = *rtx;
+				rhy = *rty;
 			}
 		}
 	}
--- 2022/09/test.rs
+++ 2022/09/test.rs
@@ -1,6 +1,6 @@
 use super::*;
 
-const EXAMPLE_INPUT :&str = "\
+const EXAMPLE_INPUT_1 :&str = "\
 R 4
 U 4
 L 3
@@ -13,6 +13,25 @@ R 2
 
 #[test]
 fn test_1() {
-	let cmds = parse(EXAMPLE_INPUT);
-	assert_eq!(visited_positions(&cmds), 13);
+	let cmds = parse(EXAMPLE_INPUT_1);
+	assert_eq!(visited_positions(&cmds, 1), 13);
+}
+
+const EXAMPLE_INPUT_2 :&str = "\
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+";
+
+#[test]
+fn test_2() {
+	let cmds = parse(EXAMPLE_INPUT_1);
+	assert_eq!(visited_positions(&cmds, 9), 1);
+	let cmds = parse(EXAMPLE_INPUT_2);
+	assert_eq!(visited_positions(&cmds, 9), 36);
 }
