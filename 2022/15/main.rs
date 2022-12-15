34a76dbe064ec34f43f77384371965780c5d230d
est31 <MTest31@outlook.com>
2022-12-15 10:47:00 +0100

    Day 2022/15: speedup part 1

--- 2022/15/main.rs
+++ 2022/15/main.rs
@@ -46,7 +46,7 @@ fn no_beacons_at(sensors :&[((i64, i64), (i64, i64))], row_num :i64) -> usize {
 		.filter(|(_sns_p, bcn_p)| bcn_p.1 == row_num)
 		.map(|(_sns_p, bcn_p)| bcn_p.0 as i64)
 		.collect::<HashSet<_>>();
-	let mut no_beacons = HashSet::new();
+	let mut no_beacons = Vec::new();
 	for (sns_p, bcn_p) in sensors {
 		let dist_x = (sns_p.0 - bcn_p.0).abs();
 		let dist_y = (sns_p.1 - bcn_p.1).abs();
@@ -58,10 +58,46 @@ fn no_beacons_at(sensors :&[((i64, i64), (i64, i64))], row_num :i64) -> usize {
 		}
 		let start_x = sns_p.0 - (dist - d);
 		let end_x = sns_p.0 + (dist - d);
-		for x in start_x..=end_x {
-			no_beacons.insert(x);
+		let mut chng_idcs = no_beacons.iter()
+			.enumerate()
+			.filter(|(_i, (st, end))| {
+				(*st..=*end).contains(&start_x) || (*st..=*end).contains(&end_x)
+					|| (start_x..=end_x).contains(st) || (start_x..=end_x).contains(end)
+			})
+			.map(|(i, itvl)| (i, *itvl))
+			.collect::<Vec<_>>();
+		println!("add {start_x}, {end_x}: {chng_idcs:?} INSIDE {no_beacons:?}");
+		if chng_idcs.len() > 0 {
+			//assert!([1, 2].contains(&chng_idcs.len()), "Wrong num: {chng_idcs:?}");
+			// There is an overlap of one or multiple intervals.
+			let start = chng_idcs[0].1.0.min(start_x);
+			let end = chng_idcs.last().unwrap().1.1.max(end_x);
+			while chng_idcs.len() > 1 {
+				// TODO use let_chains once stable
+				let i_end = chng_idcs.pop().unwrap().0;
+				let i_st = chng_idcs.last().unwrap().0;
+				for i in (i_st + 1)..=i_end {
+					no_beacons.remove(i);
+				}
+			}
+			no_beacons[chng_idcs[0].0] = (start, end);
+		} else {
+			// No overlap, just insert at the right position.
+			let mut inserted = false;
+			for i in 0..no_beacons.len() {
+				if no_beacons[i].0 > end_x {
+					no_beacons.insert(i, (start_x, end_x));
+					inserted = true;
+				}
+			}
+			if !inserted {
+				no_beacons.push((start_x, end_x));
+			}
 		}
+
 	}
-	no_beacons.retain(|x| !beacons.contains(x));
-	no_beacons.len()
+	let sum :i64 = no_beacons.iter()
+		.map(|(st, end)| end - st + 1)
+		.sum();
+	sum as usize - beacons.len()
 }
