e4f638442db7786df5e2b0ac042d71eedd3680a2
est31 <MTest31@outlook.com>
2022-12-16 09:26:10 +0100

    Day 2022/16: Add memoization
    It speeds up a lot, revealing a bug that is I think due to circles.

--- 2022/16/main.rs
+++ 2022/16/main.rs
@@ -46,23 +46,28 @@ fn parse(input :&str) -> HashMap<String, (u32, Vec<String>)> {
 		.collect::<HashMap<_, _>>()
 }
 
-fn max_release_after(valves :&HashMap<String, (u32, Vec<String>)>, valve :&str, remaining :u16) -> u32 {
+fn max_release_after(valves :&HashMap<String, (u32, Vec<String>)>, valve :&str, remaining :u16, memoized :&mut HashMap<(String, u16), u32>) -> u32 {
 	if remaining == 0 {
 		return 0;
 	}
+
+	if let Some(res) = memoized.get(&(valve.to_string(), remaining)) {
+		return *res;
+	}
+
 	let vl = &valves[valve];
 
 	let max_turned_off = vl.1.iter()
-		.map(|nxt| max_release_after(valves, nxt, remaining - 1))
+		.map(|nxt| max_release_after(valves, nxt, remaining - 1, memoized))
 		.max()
 		.unwrap_or(0);
 
 	let flow_rate = vl.0;
-	if flow_rate > 0 {
+	let res = if flow_rate > 0 {
 		let prod = flow_rate * (remaining as u32 - 1);
 		let max_turned_on = if remaining > 1 {
 			vl.1.iter()
-				.map(|nxt| max_release_after(valves, nxt, remaining - 2))
+				.map(|nxt| max_release_after(valves, nxt, remaining - 2, memoized))
 				.max()
 				.unwrap_or(0)
 		} else {
@@ -71,9 +76,12 @@ fn max_release_after(valves :&HashMap<String, (u32, Vec<String>)>, valve :&str, 
 		(max_turned_on + prod).max(max_turned_on)
 	} else {
 		max_turned_off
-	}
+	};
+	memoized.insert((valve.to_owned(), remaining), res);
+	res
 }
 
 fn max_pressure_release(valves :&HashMap<String, (u32, Vec<String>)>) -> u32 {
-	max_release_after(valves, "AA", 30)
+	let mut memoized = HashMap::new();
+	max_release_after(valves, "AA", 30, &mut memoized)
 }
