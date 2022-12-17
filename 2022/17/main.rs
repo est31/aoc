fba166310d1eb7ae02cd96ee5a23775e425339f6
est31 <MTest31@outlook.com>
2022-12-17 22:16:51 +0100

    Day 2022/17 part 2
    Do some (not 100% watertight) heuristics to detect cycles.
    There are more watertight heuristics but they are more complex.
    These give a correct result :).

--- 2022/17/main.rs
+++ 2022/17/main.rs
@@ -1,12 +1,19 @@
+use std::collections::hash_map::{HashMap, Entry};
+
 const INPUT :&str = include_str!("input");
 
 #[cfg(test)]
 mod test;
 
+const LIMIT_1 :u128 = 2022;
+const LIMIT_2 :u128 = 1000000000000;
+
 fn main() {
 	let cmds = parse(INPUT);
-	let h = tower_height(&cmds);
+	let h = tower_height(&cmds, LIMIT_1);
 	println!("Tower height: {h}");
+	let huge_h = tower_height(&cmds, LIMIT_2);
+	println!("Huge tower height: {huge_h}");
 }
 
 fn parse(input :&str) -> Vec<bool> {
@@ -86,20 +93,58 @@ fn add_at(t :Tower, fields :&mut Vec<[bool; 7]>, tx :usize, ty :usize) {
 	}
 }
 
-fn tower_height(cmds :&[bool]) -> u32 {
+fn tower_height(cmds :&[bool], limit :u128) -> u128 {
 	let mut fields = vec![[false; 7]];
-	const LIMIT :usize = 2022;
 
+	let mut cmds_ctr = 0;
 	let mut cmds_iter = cmds.iter().cycle();
 
-	for ctr in 0..LIMIT {
-		let t = TOWERS[ctr % TOWERS.len()];
+	let mut last_height = 0;
+	let mut seen = HashMap::<_, (u128, usize)>::new();
+	let mut time_travelled = false;
+
+	let mut ctr = 0;
+	let mut height_offs = 0;
+
+	while ctr < limit {
+		let ti = ctr % TOWERS.len() as u128;
+		if ti == 0 && time_travelled == false {
+			let height = fields.len() - 1;
+			let top_profile = core::array::from_fn::<_, 7, _>(|i| {
+				fields.iter()
+					.rev()
+					.enumerate()
+					.find(|(_, line)| line[i])
+					.map(|(i, _)| i)
+					.unwrap_or(0)
+			});
+			//println!("tower {ti} cmd {} diff {} top profile {top_profile:?}", cmds_ctr % cmds.len(), height - last_height);
+			let entry = seen.entry((ti, cmds_ctr % cmds.len(), height - last_height, top_profile));
+			match entry {
+				Entry::Occupied(o) => {
+					let ctr_diff = ctr - o.get().0;
+					let height_diff = height - o.get().1;
+					let applications = (limit - ctr) / ctr_diff;
+					height_offs = (applications as u128) * (height_diff as u128);
+					ctr += applications * ctr_diff;
+					time_travelled = true;
+					/*println!("Found cycle with ctr diff {ctr_diff} and height diff {height_diff}. \
+						Apply it {applications} times.");*/
+				},
+				Entry::Vacant(v) => {
+					v.insert((ctr, height));
+				},
+			}
+			last_height = height;
+		}
+		let t = TOWERS[ti as usize];
 
 		let mut x = 2;
 		let mut y = fields.len() + 2;
 
 		loop {
 			let move_right = *cmds_iter.next().unwrap();
+			cmds_ctr += 1;
 			/*if ctr <= 10 {
 				println!("Falling: x={x} y={y} right={move_right}");
 			}*/
@@ -131,8 +176,9 @@ fn tower_height(cmds :&[bool]) -> u32 {
 		println!("\nFields:");
 		print_fields(&fields[fields.len().saturating_sub(50)..]);
 		println!("------------------\n");*/
+		ctr += 1;
 	}
-	(fields.len() - 1) as _
+	fields.len() as u128 - 1 + height_offs
 }
 
 /*
--- 2022/17/test.rs
+++ 2022/17/test.rs
@@ -8,6 +8,14 @@ const EXAMPLE_INPUT :&str = "\
 fn test_1() {
 	let cmds = parse(EXAMPLE_INPUT);
 	println!("{cmds:?}");
-	let h = tower_height(&cmds);
+	let h = tower_height(&cmds, LIMIT_1);
 	assert_eq!(h, 3068);
+}
+
+#[test]
+fn test_2() {
+	let cmds = parse(EXAMPLE_INPUT);
+	println!("{cmds:?}");
+	let h = tower_height(&cmds, LIMIT_2);
+	assert_eq!(h, 1514285714288);
 }
