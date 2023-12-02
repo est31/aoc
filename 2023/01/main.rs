a49c7844cd14e74f95f99385a94f60d3d9baaebe
est31 <MTest31@outlook.com>
2023-12-02 03:06:45 +0100

    Day 2023/01 part 2

--- 2023/01/main.rs
+++ 2023/01/main.rs
@@ -4,16 +4,67 @@ const INPUT :&str = include_str!("input");
 mod test;
 
 fn main() {
-	let groups = parse(INPUT);
-	println!("sum: {}", sum(&groups));
+	let groups = parse_1(INPUT);
+	println!("sum 1: {}", sum(&groups));
+	let groups = parse_2(INPUT);
+	println!("sum 2: {}", sum(&groups));
 }
 
-fn parse(input: &str) -> Vec<u8> {
+fn parse_1(input: &str) -> Vec<u8> {
 	input.lines()
 		.map(|l| {
 			let it = l.chars().filter(|c| c.is_ascii_digit());
 			let first = it.clone().next().unwrap() as u8 - b'0';
 			let last = it.rev().next().unwrap() as u8 - b'0';
+			first * 10 + last
+		})
+		.collect()
+}
+
+fn parse_2(input: &str) -> Vec<u8> {
+	const DIGITS: &[&str] = &[
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+		"eight",
+		"nine",
+		"0",
+		"1",
+		"2",
+		"3",
+		"4",
+		"5",
+		"6",
+		"7",
+		"8",
+		"9",
+	];
+	let rev_digits = DIGITS
+		.iter()
+		.map(|s| s.chars().rev().collect::<String>())
+		.collect::<Vec<_>>();
+	fn first_digit<'a>(line: &str, digits: impl Iterator<Item = &'a str>) -> u8 {
+		let idx = digits
+			.enumerate()
+			.filter_map(|(i, s)| {
+				let offs = line.find(s)?;
+				let i = (i as u8 + 1) % 10;
+				//println!("{line}: {i} {offs}");
+				Some((i, offs))
+			})
+			.min_by_key(|(_i, offs)| *offs)
+			.unwrap().0;
+		idx
+	}
+	input.lines()
+		.map(|line| {
+			let first = first_digit(line, DIGITS.iter().map(|s| *s));
+			let line_rev = line.chars().rev().collect::<String>();
+			let last = first_digit(&line_rev, rev_digits.iter().map(|s| s.as_str()));
 			first * 10 + last
 		})
 		.collect()
--- 2023/01/test.rs
+++ 2023/01/test.rs
@@ -1,14 +1,32 @@
 use super::*;
 
-const EXAMPLE_INPUT :&str = "\
+const EXAMPLE_INPUT_1 :&str = "\
 1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet
 ";
 
+
+const EXAMPLE_INPUT_2 :&str = "\
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+";
+
 #[test]
 fn test_1() {
-	let values = parse(EXAMPLE_INPUT);
+	let values = parse_1(EXAMPLE_INPUT_1);
 	assert_eq!(142, sum(&values));
+}
+
+#[test]
+fn test_2() {
+	let values = parse_2(EXAMPLE_INPUT_2);
+	assert_eq!([29, 83, 13, 24, 42, 14, 76], values.as_slice());
+	assert_eq!(281, sum(&values));
 }
