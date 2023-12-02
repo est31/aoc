ec9c79c649affed1a2247e013bbadc2e01953610
est31 <MTest31@outlook.com>
2023-12-02 06:20:09 +0100

    Day 2023/02 part 1

---
+++ 2023/02/main.rs
@@ -0,0 +1,71 @@
+use std::str::FromStr;
+
+const INPUT :&str = include_str!("input");
+
+#[cfg(test)]
+mod test;
+
+fn main() {
+	let groups = parse(INPUT);
+	println!("sum: {}", possible_games_id_sum(&groups));
+}
+
+struct Rgb {
+	red: u16,
+	green: u16,
+	blue: u16,
+}
+
+impl Rgb {
+	fn parse(s: &str) -> Self {
+		let mut red = 0;
+		let mut green = 0;
+		let mut blue = 0;
+		for component in s.split(',') {
+			let component = component.trim();
+			if component.len() == 0 {
+				continue;
+			}
+			let mut sub_cmp = component.split(' ');
+			let (Some(val_st), Some(color_st)) = (sub_cmp.next(), sub_cmp.next()) else {
+				panic!("Invalid component '{component}'");
+			};
+			let val = u16::from_str(val_st).unwrap();
+			match color_st {
+				"red" => red = val,
+				"green" => green = val,
+				"blue" => blue = val,
+				_ => panic!("Invalid color '{color_st}'"),
+			}
+		}
+		Self { red, green, blue }
+	}
+	fn possible(&self) -> bool {
+		self.red <= 12 && self.green <= 13 && self.blue <= 14
+	}
+}
+
+fn parse(input: &str) -> Vec<Vec<Rgb>> {
+	input.lines()
+		.map(|l| l.trim())
+		.filter(|l| !l.is_empty())
+		.map(|l| {
+			let revs = l.split(':').nth(1).unwrap();
+			revs.split(';')
+				.map(|r| Rgb::parse(r))
+				.collect::<Vec<_>>()
+		})
+		.collect::<Vec<_>>()
+}
+
+fn possible_games_id_sum(games: &[Vec<Rgb>]) -> u16 {
+	let sum: u16 = games.iter()
+		.enumerate()
+		.filter(|(_i, revelations)| {
+			revelations.iter()
+				.all(|rev| rev.possible())
+		})
+		.map(|(i, _rev)| i as u16 + 1)
+		.sum();
+	sum
+}
---
+++ 2023/02/test.rs
@@ -0,0 +1,15 @@
+use super::*;
+
+const EXAMPLE_INPUT :&str = "\
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+";
+
+#[test]
+fn test() {
+	let values = parse(EXAMPLE_INPUT);
+	assert_eq!(8, possible_games_id_sum(&values));
+}
