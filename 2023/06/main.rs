d0dde18bf706191372fca434e20dc1e1cbc17e5a
est31 <MTest31@outlook.com>
2023-12-06 20:22:40 +0100

    Day 2023/06 part 2 bruteforce

--- 2023/06/main.rs
+++ 2023/06/main.rs
@@ -8,40 +8,63 @@ mod test;
 fn main() {
 	let times_max_dist = parse(INPUT);
 	println!("number product: {}", numbers_product(&times_max_dist));
+	let corr = parse_kernig_correction(INPUT);
+	println!("kerning corrected: {}", number_of_beating_hold_times(corr.0, corr.1));
 }
 
-fn parse(input :&str) -> Vec<(u32, u32)> {
+fn parse(input :&str) -> Vec<(u64, u64)> {
 	let mut lines = input.lines()
 		.map(|l| l.trim());
 	let times = lines.next().unwrap();
 	let max_distances = lines.next().unwrap();
 	let times = times.split_whitespace()
 		.skip(1)
-		.map(|c| u32::from_str(c).unwrap())
+		.map(|c| u64::from_str(c).unwrap())
 		.collect::<Vec<_>>();
 	let max_distances = max_distances.split_whitespace()
 		.skip(1)
-		.map(|c| u32::from_str(c).unwrap())
+		.map(|c| u64::from_str(c).unwrap())
 		.collect::<Vec<_>>();
 	times.into_iter()
 		.zip(max_distances.into_iter())
 		.collect::<Vec<_>>()
 }
 
-fn number_of_beating_hold_times_bf(time :u32, dist :u32) -> u32 {
-	// How many h exist so that h*(time - h) > dist?
-	(1..time)
-		.filter(|&h| h * (time - h) > dist)
-		.count() as u32
+fn parse_kernig_correction(input :&str) -> (u64, u64) {
+	let mut lines = input.lines()
+		.map(|l| l.trim());
+	let times = lines.next().unwrap();
+	let max_distances = lines.next().unwrap();
+
+	fn process(input :&str) -> u64 {
+		let s = input.split(':')
+			.nth(1)
+			.unwrap();
+		let s = s.split_whitespace()
+			.collect::<String>();
+		u64::from_str(&s).unwrap()
+	}
+	let times = process(&times);
+	let max_distances = process(&max_distances);
+
+	(times, max_distances)
 }
 
-fn number_of_beating_hold_times(time :u32, dist :u32) -> u32 {
+fn number_of_beating_hold_times_bf(time :u64, dist :u64) -> u64 {
+	// How many h exist so that h*(time - h) > dist?
+	// brute force search
+	(1..time)
+		.filter(|&h| h * (time - h) > dist)
+		.count() as u64
+}
+
+fn number_of_beating_hold_times(time :u64, dist :u64) -> u64 {
 	// question: how many h exist so that h*(time - h) > dist?
 	// h*time - h*h - dist = 0
 	number_of_beating_hold_times_bf(time, dist)
 }
 
-fn numbers_product(times_max_dist :&[(u32, u32)]) -> u32 {
+fn numbers_product(times_max_dist :&[(u64, u64)]) -> u64 {
 	times_max_dist.iter()
 		.map(|(time, max_dist)| number_of_beating_hold_times(*time, *max_dist))
 		.product()
--- 2023/06/test.rs
+++ 2023/06/test.rs
@@ -6,6 +6,14 @@ Distance:  9  40  200
 ";
 
 #[test]
+fn test_bf() {
+	assert_eq!(number_of_beating_hold_times_bf(7, 9), 4);
+	assert_eq!(number_of_beating_hold_times_bf(15, 40), 8);
+	assert_eq!(number_of_beating_hold_times_bf(30, 200), 9);
+	assert_eq!(number_of_beating_hold_times_bf(71530, 940200), 71503);
+}
+
+#[test]
 fn test() {
 	let times_max_dist = parse(EXAMPLE_INPUT);
 	println!("{times_max_dist:?}");
@@ -13,4 +21,5 @@ fn test() {
 	assert_eq!(number_of_beating_hold_times(15, 40), 8);
 	assert_eq!(number_of_beating_hold_times(30, 200), 9);
 	assert_eq!(numbers_product(&times_max_dist), 288);
+	assert_eq!(number_of_beating_hold_times(71530, 940200), 71503);
 }
