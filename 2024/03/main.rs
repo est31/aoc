70b4114aab612b37a090725a56a714d0264221cb
est31 <MTest31@outlook.com>
2024-12-04 00:03:38 +0100

    Day 2024/03 part 2

--- 2024/03/main.rs
+++ 2024/03/main.rs
@@ -6,38 +6,52 @@ const INPUT :&str = include_str!("input");
 mod test;
 
 fn main() {
-	println!("sum of muls: {}", sum_of_muls(INPUT));
+	println!("sum of muls: {}", sum_of_muls(INPUT, false));
+	println!("sum of muls (with enablement): {}", sum_of_muls(INPUT, true));
 }
 
-fn sum_of_muls(s: &str) -> u32 {
+fn sum_of_muls(s: &str, modes: bool) -> u32 {
+	let mut components = Vec::new();
+
+	let mut dos = s.split("do()");
+	while let Some(do_component) = dos.next() {
+		let mut donts = do_component.splitn(2, "don't()");
+		components.push((true, donts.next().unwrap()));
+		if let Some(dont) = donts.next() {
+			components.push((false, dont));
+		}
+	}
+
 	let mut muls = Vec::new();
-	let mut it = s.split("mul");
-	let mut first = true;
-	while let Some(component) = it.next() {
-		if first {
-			first = false;
-			continue;
-		}
-		let Some(first_n) = component.strip_prefix("(") else {
-			continue
-		};
-		if !first_n.contains(')') {
-			continue;
-		}
-		let nums = first_n.split(')').next().unwrap();
-		let mut nums_iter = nums.split(',');
-		let (Some(first), Some(second)) = (nums_iter.next(), nums_iter.next()) else {
-			continue
-		};
-		let (Ok(first), Ok(second)) = (u32::from_str(first), u32::from_str(second)) else {
-			continue
-		};
+	for (enabled, component) in components {
+		let mut it = component.split("mul");
+		// The first component has no 'mul' prefix
+		it.next().unwrap();
+		while let Some(component) = it.next() {
+			let Some(first_n) = component.strip_prefix("(") else {
+				continue
+			};
+			if !first_n.contains(')') {
+				continue;
+			}
+			let nums = first_n.split(')').next().unwrap();
+			let mut nums_iter = nums.split(',');
+			let (Some(first), Some(second)) = (nums_iter.next(), nums_iter.next()) else {
+				continue
+			};
+			let (Ok(first), Ok(second)) = (u32::from_str(first), u32::from_str(second)) else {
+				continue
+			};
 
-		if first >= 1000 || second >= 1000 {
-			continue;
+			if first >= 1000 || second >= 1000 {
+				continue;
+			}
+
+			if enabled || !modes {
+				muls.push((first, second));
+			}
 		}
 
-		muls.push((first, second));
 	}
 	muls.iter()
 		.map(|(v0, v1)| *v0 * *v1)
--- 2024/03/test.rs
+++ 2024/03/test.rs
@@ -4,7 +4,12 @@ const EXAMPLE_INPUT_1 :&str = "\
 xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 ";
 
+
+const EXAMPLE_INPUT_2 :&str = "\
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+";
 #[test]
 fn test_1() {
-	assert_eq!(161, sum_of_muls(EXAMPLE_INPUT_1));
+	assert_eq!(161, sum_of_muls(EXAMPLE_INPUT_1, false));
+	assert_eq!(48, sum_of_muls(EXAMPLE_INPUT_2, true));
 }
