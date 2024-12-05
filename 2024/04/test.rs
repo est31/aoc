3ccc0db362a2db00a865ab99a798c73f288427d7
est31 <MTest31@outlook.com>
2024-12-05 09:28:58 +0100

    More tests

--- 2024/04/test.rs
+++ 2024/04/test.rs
@@ -22,6 +22,83 @@ MAMMMXMMMM
 MXMXAXMASX
 ";
 
+
+const EXAMPLE_INPUT_2 :&str = "\
+....XXMAS.
+.SAMXMS...
+...S..A...
+..A.A.MS.X
+XMASAMX.MM
+X.....XA.A
+S.S.S.S.SS
+.A.A.A.A.A
+..M.M.M.MM
+.X.X.XMASX
+";
+
+const EXAMPLE_INPUT_2_H :&str = "\
+S.S.S.S.SS
+.A.A.A.A.A
+..M.M.M.MM
+.X.X.XMASX
+";
+
+const EXAMPLE_INPUT_3_1 :&str = "\
+XMAS
+....
+....
+....
+";
+
+const EXAMPLE_INPUT_3_2 :&str = "\
+X...
+M...
+A...
+S...
+";
+
+const EXAMPLE_INPUT_3_3 :&str = "\
+SAMX
+....
+....
+....
+";
+
+const EXAMPLE_INPUT_3_4 :&str = "\
+S...
+A...
+M...
+X...
+";
+
+const EXAMPLE_INPUT_3_5 :&str = "\
+X...
+.M..
+..A.
+...S
+";
+
+const EXAMPLE_INPUT_3_6 :&str = "\
+S...
+.A..
+..M.
+...X
+";
+
+const EXAMPLE_INPUT_3_7 :&str = "\
+...S
+..A.
+.M..
+X...
+";
+
+const EXAMPLE_INPUT_3_8 :&str = "\
+...X
+..M.
+.A..
+S...
+";
+
 #[test]
 fn test_0() {
 	assert_eq!(4, count_xmas(EXAMPLE_INPUT_0));
@@ -30,5 +107,19 @@ fn test_0() {
 
 #[test]
 fn test_1() {
-	assert_eq!(18, count_xmas(EXAMPLE_INPUT_1));
+	//assert_eq!(18, count_xmas(EXAMPLE_INPUT_1));
+	//assert_eq!(18, count_xmas(EXAMPLE_INPUT_2));
+	assert_eq!(8, count_xmas(EXAMPLE_INPUT_2_H));
+}
+
+#[test]
+fn test_1_3() {
+	assert_eq!(1, count_xmas(EXAMPLE_INPUT_3_1));
+	assert_eq!(1, count_xmas(EXAMPLE_INPUT_3_2));
+	assert_eq!(1, count_xmas(EXAMPLE_INPUT_3_3));
+	assert_eq!(1, count_xmas(EXAMPLE_INPUT_3_4));
+	assert_eq!(1, count_xmas(EXAMPLE_INPUT_3_5));
+	assert_eq!(1, count_xmas(EXAMPLE_INPUT_3_6));
+	assert_eq!(1, count_xmas(EXAMPLE_INPUT_3_7));
+	assert_eq!(1, count_xmas(EXAMPLE_INPUT_3_8));
 }
