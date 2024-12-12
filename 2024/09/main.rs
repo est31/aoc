fea0d6e91c526a7ccd9ea10173945e8f11578da2
est31 <MTest31@outlook.com>
2024-12-12 13:37:56 +0100

    Day 2024/09 part 2

--- 2024/09/main.rs
+++ 2024/09/main.rs
@@ -1,5 +1,3 @@
-use std::collections::{HashMap, HashSet};
-
 const INPUT :&str = include_str!("input");
 
 #[cfg(test)]
@@ -8,11 +6,12 @@ mod test;
 fn main() {
 	let map = parse(INPUT);
 	println!("defrag checksum: {}", defrag_checksum(&map));
+	println!("defrag checksum (whole only): {}", defrag_checksum_whole_only(&map));
 }
 
 macro_rules! dprint {
 	($($args:expr),*) => {
-		//if false
+		if false
 			{ print!($($args),*); }
 	};
 }
@@ -46,7 +45,7 @@ fn defrag_checksum(map: &[u8]) -> u128 {
 	let mut filled_before = 0;
 	advance_filled_before(&disk_chunks, &mut filled_before);
 	while filled_before < disk_chunks.len() - 1 {
-		dprint!("filled_before = {filled_before}\n");
+		dprint!("filled_before={filled_before}\n");
 		let (last_cnt, last_v) = *disk_chunks.last().unwrap();
 		let avail_dest = disk_chunks[filled_before].0;
 		let avail_orig = last_cnt;
@@ -66,6 +65,86 @@ fn defrag_checksum(map: &[u8]) -> u128 {
 		if disk_chunks.last_mut().unwrap().0 == 0 {
 			dprint!("  -> pop last chunk\n");
 			disk_chunks.pop();
+		}
+		advance_filled_before(&disk_chunks, &mut filled_before);
+	}
+	dprint!("resulting disk_chunks: {disk_chunks:?}\n");
+	// Checksum
+	let mut sum = 0;
+	let mut pos = 0;
+	for (cnt, v) in disk_chunks.iter() {
+		for _ in 0..*cnt {
+			sum += pos * (*v as u128).saturating_sub(1);
+			pos += 1;
+		}
+	}
+	sum
+}
+
+fn defrag_checksum_whole_only(map: &[u8]) -> u128 {
+	let mut disk_chunks = Vec::new();
+	// Preparation
+	let mut file_ctr = 0;
+	for (i, count) in map.iter().enumerate() {
+		let id_num = if i % 2 == 0 {
+			file_ctr += 1;
+			file_ctr
+		} else {
+			0
+		};
+		disk_chunks.push((*count, id_num));
+	}
+	dprint!("before defrag: {disk_chunks:?}\n");
+	// Defrag
+	let mut filled_before = 0;
+	advance_filled_before(&disk_chunks, &mut filled_before);
+	let mut orig_i = disk_chunks.len() - 1;
+	let mut orig_v = disk_chunks[orig_i].1;
+	'outer: while filled_before < orig_i {
+		while orig_i > filled_before {
+			let candidate_v = disk_chunks[orig_i].1;
+			if candidate_v != 0 && candidate_v <= orig_v {
+				orig_v = candidate_v;
+				break;
+			}
+			orig_i -= 1;
+		}
+		if orig_i == filled_before {
+			break;
+		}
+		let (avail_orig, _) = disk_chunks[orig_i];
+		dprint!("filled_before={filled_before}, orig_i={orig_i}, avail=({avail_orig},{orig_v})\n");
+
+		let mut dest_pos = filled_before;
+		while disk_chunks[dest_pos].0 < avail_orig {
+			dest_pos += 1;
+			advance_filled_before(&disk_chunks, &mut dest_pos);
+			if dest_pos >= orig_i {
+				dprint!("  -> not moved\n");
+				orig_i -= 1;
+				continue 'outer;
+			}
+		}
+		let avail_dest = disk_chunks[dest_pos].0;
+		let moved_cnt;
+		dprint!("  -> dest_pos={dest_pos}, avail_dest={avail_dest}, avail_orig={avail_orig}\n");
+		if avail_dest < avail_orig {
+			panic!("Not supposed to happen");
+		} else if avail_dest == avail_orig {
+			moved_cnt = avail_dest;
+			dprint!("  -> update ({}, {}) to ({moved_cnt}, {orig_v})\n", disk_chunks[dest_pos].0, disk_chunks[dest_pos].1);
+			disk_chunks[dest_pos] = (moved_cnt, orig_v);
+		} else {
+			moved_cnt = avail_orig;
+			disk_chunks[dest_pos].0 -= moved_cnt;
+			disk_chunks.insert(dest_pos, (moved_cnt, orig_v));
+			orig_i += 1;
+			dprint!("  -> insert ({moved_cnt}, {orig_v})\n");
+		}
+		dprint!("  -> set cnt={moved_cnt} at orig_i={orig_i} to 0\n");
+		disk_chunks[orig_i].1 = 0;
+		if disk_chunks[orig_i].0 != moved_cnt {
+			panic!("Didn't move entire file");
 		}
 		advance_filled_before(&disk_chunks, &mut filled_before);
 	}
--- 2024/09/test.rs
+++ 2024/09/test.rs
@@ -10,3 +10,9 @@ fn test_1() {
 	let eqs = parse(EXAMPLE_INPUT_1);
 	assert_eq!(1928, defrag_checksum(&eqs));
 }
+
+#[test]
+fn test_2() {
+	let eqs = parse(EXAMPLE_INPUT_1);
+	assert_eq!(2858, defrag_checksum_whole_only(&eqs));
+}
