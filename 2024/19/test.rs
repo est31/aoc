2f1df3e08e5363b891c7f5d06df60dd4ed445199
est31 <MTest31@outlook.com>
2024-12-23 23:10:56 +0100

    Extend test

--- 2024/19/test.rs
+++ 2024/19/test.rs
@@ -21,6 +21,27 @@ fn test_1() {
 
 #[test]
 fn test_2() {
-	let twl = parse(EXAMPLE_INPUT_1);
+	let mut twl = parse(EXAMPLE_INPUT_1);
+	let desired = std::mem::take(&mut twl.desired);
+
+	twl.desired = vec![desired[0].clone()];
+	assert_eq!(2, twl.sum_possible());
+
+	twl.desired = vec![desired[1].clone()];
+	assert_eq!(1, twl.sum_possible());
+
+	twl.desired = vec![desired[2].clone()];
+	assert_eq!(4, twl.sum_possible());
+
+	twl.desired = vec![desired[3].clone()];
+	assert_eq!(6, twl.sum_possible());
+
+	twl.desired = vec![desired[4].clone()];
+	assert_eq!(1, twl.sum_possible());
+
+	twl.desired = vec![desired[5].clone()];
+	assert_eq!(2, twl.sum_possible());
+
+	twl.desired = desired;
 	assert_eq!(16, twl.sum_possible());
 }
