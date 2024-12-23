09b71f3e7be7fcd1dfb2fab02c3dbc3f3962b8ad
est31 <MTest31@outlook.com>
2024-12-23 01:22:15 +0100

    Day 2024/18 part 1

---
+++ 2024/18/main.rs
@@ -0,0 +1,105 @@
+use std::collections::{BTreeSet, HashSet};
+use std::str::FromStr;
+
+const INPUT :&str = include_str!("input");
+
+#[cfg(test)]
+mod test;
+
+fn main() {
+	let mem = parse(INPUT);
+	println!("min steps: {}", mem.min_steps());
+}
+
+#[derive(Clone, PartialEq, Eq)]
+struct Memory {
+	height :usize,
+	width :usize,
+	corruptions :Vec<(usize, usize)>,
+}
+
+fn parse(s :&str) -> Memory {
+	parse_sized(s, 71)
+}
+
+fn parse_sized(s :&str, size :usize) -> Memory {
+	let corruptions = s.trim()
+		.lines()
+		.map(str::trim)
+		.map(|l| {
+			let mut x_y_it = l.split(',')
+				.map(|v| usize::from_str(v).unwrap());
+			(x_y_it.next().unwrap(), x_y_it.next().unwrap())
+		})
+		.collect::<Vec<_>>();
+	let height = size;
+	let width = size;
+
+	Memory {
+		height,
+		width,
+		corruptions,
+	}
+}
+
+macro_rules! dprint {
+	($($args:expr),*) => {
+		if false
+			{ print!($($args),*); }
+	};
+}
+
+#[inline(always)]
+fn neighs(p: (usize, usize), height :usize, width :usize) -> Vec<(usize, usize)> {
+	let mut neighs = Vec::with_capacity(4);
+	if p.0 > 0 {
+		neighs.push((p.0 - 1, p.1));
+	}
+	if p.1 > 0 {
+		neighs.push((p.0, p.1 - 1));
+	}
+	if p.0 + 1 < height {
+		neighs.push((p.0 + 1, p.1));
+	}
+	if p.1 + 1 < width {
+		neighs.push((p.0, p.1 + 1));
+	}
+	neighs
+}
+
+impl Memory {
+	fn min_steps(&self) -> usize {
+		self.min_steps_after(1024)
+	}
+	fn min_steps_after(&self, ms :usize) -> usize {
+		let mut fields = vec![vec![false; self.width]; self.height];
+
+		for corr in self.corruptions.iter().take(ms) {
+			fields[corr.0][corr.1] = true;
+		}
+
+		let start_pos = (0, 0);
+		let end_pos = (self.height - 1, self.width - 1);
+
+		let mut handled = HashSet::new();
+		let mut unhandled = BTreeSet::new();
+		unhandled.insert((0, start_pos));
+		while let Some((cost, pos)) = unhandled.pop_first() {
+			if fields[pos.0][pos.1] {
+				continue;
+			}
+			if !handled.insert(pos) {
+				continue;
+			}
+			if pos == end_pos {
+				return cost;
+			}
+			let neighs = neighs(pos, self.height, self.width);
+			for new_pos in neighs {
+				unhandled.insert((cost + 1, new_pos));
+			}
+		}
+
+		panic!("didn't get to end")
+	}
+}
---
+++ 2024/18/test.rs
@@ -0,0 +1,36 @@
+use super::*;
+
+const EXAMPLE_INPUT_1 :&str = "\
+5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0
+";
+
+#[test]
+fn test_1_1() {
+	let mem = parse_sized(EXAMPLE_INPUT_1, 7);
+	assert_eq!(22, mem.min_steps_after(12));
+}
+
