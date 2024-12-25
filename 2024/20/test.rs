aa51273ba23693787d087cf6bef9dd317a488295
est31 <MTest31@outlook.com>
2024-12-25 03:04:26 +0100

    extend test

--- 2024/20/test.rs
+++ 2024/20/test.rs
@@ -40,12 +40,19 @@ fn test_1() {
 #[test]
 fn test_neighs_manhattan() {
 	let c = (10, 100);
-	fn tm(c :Pos, positions :&[(Pos, usize)]) -> usize {
+	fn tm(c :Pos, max_dist :usize, positions :&[(Pos, usize)]) -> usize {
 		let pos_set = positions.iter().collect::<HashSet<_>>();
 		assert_eq!(pos_set.len(), positions.len());
+		for p in positions {
+			let mut sum = 0;
+			sum += (c.0 as isize - p.0.0 as isize).abs();
+			sum += (c.1 as isize - p.0.1 as isize).abs();
+			assert_eq!(sum as usize, p.1);
+			assert!(max_dist as isize >= sum);
+		}
 		positions.len()
 	}
-	assert_eq!(tm(c, &neighs_manhattan(c, 1000, 1000, 1)), 4);
-	assert_eq!(tm(c, &neighs_manhattan(c, 1000, 1000, 2)), 12);
-	assert_eq!(tm(c, &neighs_manhattan(c, 1000, 1000, 3)), 24);
+	assert_eq!(tm(c, 1, &neighs_manhattan(c, 1000, 1000, 1)), 4);
+	assert_eq!(tm(c, 2, &neighs_manhattan(c, 1000, 1000, 2)), 12);
+	assert_eq!(tm(c, 3, &neighs_manhattan(c, 1000, 1000, 3)), 24);
 }
