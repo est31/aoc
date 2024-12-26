1e4982c88bb610cf9702552c7fbc2e228060278e
est31 <MTest31@outlook.com>
2024-12-26 03:50:15 +0100

    Day 2024/21 part 1 buggy implementation

--- 2024/21/main.rs
+++ 2024/21/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 const INPUT :&str = include_str!("input");
 
 #[cfg(test)]
@@ -14,6 +16,25 @@ enum PosNum {
 	A,
 }
 
+impl PosNum {
+	fn coord(&self) -> (i8, i8) {
+		match self {
+			PosNum::Digit(0) => (1, 3),
+			PosNum::A => (2, 3),
+			PosNum::Digit(1) => (0, 2),
+			PosNum::Digit(2) => (1, 2),
+			PosNum::Digit(3) => (2, 2),
+			PosNum::Digit(4) => (0, 1),
+			PosNum::Digit(5) => (1, 1),
+			PosNum::Digit(6) => (2, 1),
+			PosNum::Digit(7) => (0, 0),
+			PosNum::Digit(8) => (0, 0),
+			PosNum::Digit(9) => (0, 0),
+			PosNum::Digit(_) => panic!("Invalid digit"),
+		}
+	}
+}
+
 #[derive(Copy, Clone, PartialEq, Eq, Debug, Hash)]
 enum Pos {
 	Up,
@@ -23,11 +44,28 @@ enum Pos {
 	Right,
 }
 
-#[derive(Clone, PartialEq, Eq)]
-struct State {
-	pos_numeric :PosNum,
-	pos_robot_1 :Pos,
-	pos_robot_2 :Pos,
+impl Pos {
+	fn coord(&self) -> (i8, i8) {
+		match self {
+			Pos::Up => (1, 0),
+			Pos::A => (2, 0),
+			Pos::Left => (0, 1),
+			Pos::Down => (1, 1),
+			Pos::Right => (2, 1),
+		}
+	}
+}
+
+impl std::fmt::Display for Pos {
+	fn fmt(&self, f :&mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+		match self {
+			Pos::Up => write!(f, "^"),
+			Pos::A => write!(f, "A"),
+			Pos::Left => write!(f, "<"),
+			Pos::Down => write!(f, "v"),
+			Pos::Right => write!(f, ">"),
+		}
+	}
 }
 
 fn parse(s :&str) -> Vec<Vec<PosNum>> {
@@ -46,8 +84,89 @@ fn parse(s :&str) -> Vec<Vec<PosNum>> {
 		.collect::<Vec<_>>()
 }
 
+macro_rules! dprint {
+	($($args:expr),*) => {
+		//if false
+			{ print!($($args),*); }
+	};
+}
+
+fn shortest_for_pos_num(code :&[PosNum]) -> Vec<Pos> {
+	let mut cmds = Vec::new();
+	for wnd in code.windows(2) {
+		let cd_from = wnd[0];
+		let cd_to = wnd[1];
+		let coord_from = cd_from.coord();
+		let coord_to = cd_to.coord();
+		let x_cmds = match coord_from.0.cmp(&coord_to.0) {
+			Ordering::Less => {
+				vec![Pos::Right; (coord_to.0 - coord_from.0) as usize]
+			}
+			Ordering::Equal => {
+				vec![]
+			}
+			Ordering::Greater => {
+				vec![Pos::Left; (coord_from.0 - coord_to.0) as usize]
+			}
+		};
+		match coord_from.1.cmp(&coord_to.1) {
+			Ordering::Less => {
+				cmds.extend_from_slice(&x_cmds);
+				cmds.extend_from_slice(&vec![Pos::Down; (coord_to.1 - coord_from.1) as usize]);
+			}
+			Ordering::Equal => (),
+			Ordering::Greater => {
+				cmds.extend_from_slice(&vec![Pos::Up; (coord_from.1 - coord_to.1) as usize]);
+				cmds.extend_from_slice(&x_cmds);
+			}
+		};
+		cmds.push(Pos::A);
+	}
+	dprint!("numpad cmds: {}\n", cmds.iter().map(|s| format!("{s}")).collect::<String>());
+	cmds
+}
+
+fn shortest_remote_one(code :&[Pos]) -> Vec<Pos> {
+	let mut cmds = Vec::new();
+	for wnd in code.windows(2) {
+		let cd_from = wnd[0];
+		let cd_to = wnd[1];
+		let coord_from = cd_from.coord();
+		let coord_to = cd_to.coord();
+		let x_cmds = match coord_from.0.cmp(&coord_to.0) {
+			Ordering::Less => {
+				vec![Pos::Right; (coord_to.0 - coord_from.0) as usize]
+			}
+			Ordering::Equal => {
+				vec![]
+			}
+			Ordering::Greater => {
+				vec![Pos::Left; (coord_from.0 - coord_to.0) as usize]
+			}
+		};
+		match coord_from.1.cmp(&coord_to.1) {
+			Ordering::Less => {
+				cmds.extend_from_slice(&vec![Pos::Down; (coord_to.1 - coord_from.1) as usize]);
+				cmds.extend_from_slice(&x_cmds);
+			}
+			Ordering::Equal => (),
+			Ordering::Greater => {
+				cmds.extend_from_slice(&x_cmds);
+				cmds.extend_from_slice(&vec![Pos::Up; (coord_from.1 - coord_to.1) as usize]);
+			}
+		};
+		cmds.push(Pos::A);
+	}
+	dprint!("robot cmds: {}\n", cmds.iter().map(|s| format!("{s}")).collect::<String>());
+	cmds
+}
+
 fn shortest_press_seq(code :&[PosNum]) -> Vec<Pos> {
-	todo!()
+	let code_0 = shortest_for_pos_num(code);
+	let code_1 = shortest_remote_one(&code_0);
+	let code_2 = shortest_remote_one(&code_1);
+	let final_code = shortest_remote_one(&code_2);
+	final_code
 }
 
 fn code_to_num(code :&[PosNum]) -> u32 {
@@ -68,14 +187,4 @@ fn sum_complexities(list :&[Vec<PosNum>]) -> u32 {
 	list.iter()
 		.map(|c| complexity(c))
 		.sum()
-}
-
-macro_rules! dprint {
-	($($args:expr),*) => {
-		if false
-			{ print!($($args),*); }
-	};
-}
-
-impl State {
 }
